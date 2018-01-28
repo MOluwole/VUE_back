ad3f160af000a4348ff909e89644ae873f558fdc
MOluwole <oluwole564@gmail.com>
2018-01-28 21:01:33 +0100

    initial commit

--- config/database.js
+++ config/database.js
@@ -45,14 +45,28 @@ module.exports = {
   | npm i --save mysql
   |
   */
+  // mysql: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: Env.get('DB_HOST', 'localhost'),
+  //     port: Env.get('DB_PORT', ''),
+  //     user: Env.get('DB_USER', 'root'),
+  //     password: Env.get('DB_PASSWORD', ''),
+  //     database: Env.get('DB_DATABASE', 'adonis')
+  //   },
+  //   debug: Env.get('DB_DEBUG', false)
+  // },
+
+  // mysql://:@/?reconnect=true
+
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', 'b77ebbbac301a4'),
+      password: Env.get('DB_PASSWORD', '35e520f2'),
+      database: Env.get('DB_DATABASE', 'heroku_242c76dc98cd83c')
     },
     debug: Env.get('DB_DEBUG', false)
   },
